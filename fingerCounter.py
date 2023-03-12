6a26bf04ceddf0ff0eb3f7397f2c4a44f90cbe89
space monkey <yashcgosavi@gmail.com>
2023-03-13 02:25:29 +0530

    add volumeHandControl.py

--- fingerCounter.py
+++ fingerCounter.py
@@ -1,20 +1,40 @@
 import cv2
 import os
 import time
+import HandTrackingModule as htm
 
+pTime = 0
 cap = cv2.VideoCapture(0)
-# cap.set(3, 640)
-# cap.set(4, 480)
-x = 0
-while True:
+wcam = 640
+hcam = 480
 
+cap.set(3, wcam)
+cap.set(4, hcam)
+x = 0
+
+detector = htm.HandDetector(detectionCon=0.75)
+
+tipIds = [4, 8, 12, 16, 20]
+
+while True:
     _, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw = False)
+    print(lmList)
+    if lmList:
+
+        for id in tipIds:
+            fingers = []
+            if lmList[id][2] < lmList[id - 2][2]:
+                print("index finger open")
+
+    cTime = time.time()
+    fps=1/(cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f"FPS: {int(fps)}", (400, 70), cv2.FONT_HERSHEY_PLAIN,
+                3, (255, 0, 0), 3)
+
     cv2.imshow("Image", img)
-    if cv2.waitKey(1) & 0xFF == ord('y'):
-        cv2.imwrite(f'c{x}.png', img)
-        x+=1
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cv2.destroyAllWindows()
-cap.release()
+    cv2.waitKey(1)
 
